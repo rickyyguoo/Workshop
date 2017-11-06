72775a84b764679c5de42319b35197668956e7aa
RickyGuo <rickyyguoo@gmail.com>
2017-11-06 11:06:32 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const http = express();
+
+http.set('view engine', 'ejs');
+http.set('views', 'views');
+
+http.use('/fonts', express.static('static/font-awesome-4.7.0/fonts'));
+http.use('/static', express.static('static'));
+
+http.get('/', (req, res) => {
+    res.render('index');
+});
+
+http.get('/index', (req, res) =>{
+	res.render('index');
+});
+
+http.get('/events', (req, res) =>{
+	res.render('events');
+});
+
+http.get('/about', (req, res) =>{
+	res.render('about');
+});
+
+http.listen(3000, () => {
+    console.log('listening on 3000...');
+});
